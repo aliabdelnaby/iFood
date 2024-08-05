c0b1cd33cc0d7e4010c05d03f78276a347697e9d
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-05 22:56:36 +0300

    first commit & App colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF0D8754);
+  static const Color searchColor = Color(0xFFE9E8E8);
+  static const Color likedColor = Color(0xFFC51B1B);
+  static const Color menuGreyColor = Color(0xFFD9D9D9);
+  static const Color ratingColor = Color(0xFFF9E20D);
+  static const Color unlikedColor = Color(0xFFF9E20D);
+  static const Color ratingGreyColor = Color(0xFFDBDBDB);
+  static const Color greyColor = Color(0xFF706F6F);
+  static Color cardShadowColor = Colors.black.withOpacity(0.25);
+  static const Color emailColor = Color(0xFF737373);
+  static const Color logOutColor = Color(0xFFF6F6F6);
+}
