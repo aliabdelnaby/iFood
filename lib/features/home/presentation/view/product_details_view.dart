dcfb0dd8dc119fcb8f6091711c6077d948625d4c
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-07 17:53:01 +0300

    Product Details Bottom Navigation Bar

--- lib/features/home/presentation/view/product_details_view.dart
+++ lib/features/home/presentation/view/product_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ifood/features/home/presentation/widget/product_details/product_details_nav_bar.dart';
 
 class ProductDetailsView extends StatelessWidget {
   const ProductDetailsView({super.key});
@@ -6,6 +7,7 @@ class ProductDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const ProductDetailsBottomNavigationBar(),
       body: Container(),
     );
   }
---
+++ lib/features/home/presentation/widget/product_details/product_details_nav_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:ifood/core/utils/app_colors.dart';
+import 'package:ifood/core/utils/app_text_styles.dart';
+
+class ProductDetailsBottomNavigationBar extends StatelessWidget {
+  const ProductDetailsBottomNavigationBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding:
+          const EdgeInsetsDirectional.symmetric(horizontal: 23, vertical: 10),
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: const Icon(
+              Icons.favorite,
+              size: 30,
+              color: AppColors.primGreyColor,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Add to cart',
+                style: AppStyles.style18w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
