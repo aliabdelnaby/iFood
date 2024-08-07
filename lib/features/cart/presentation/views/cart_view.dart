8b0bbb6a912721afc6f7ef48a208c7d03a96165c
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-08 00:10:30 +0300

    Cart AppBar

--- lib/features/cart/presentation/views/cart_view.dart
+++ lib/features/cart/presentation/views/cart_view.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:ifood/core/utils/app_colors.dart';
+import 'package:ifood/core/utils/app_text_styles.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('Cart View'),
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.scaffoldBackgroundColor,
+        title: Text(
+          'Cart',
+          style: AppStyles.style28w700.copyWith(
+            color: AppColors.blackColor,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text('Cart View'),
+      ),
+    );
   }
 }
