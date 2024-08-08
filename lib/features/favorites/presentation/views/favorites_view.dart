9a05cb234377ff1492f6113691163d379efb374b
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-08-08 11:52:25 +0300

    Favorites AppBar

--- lib/features/favorites/presentation/views/favorites_view.dart
+++ lib/features/favorites/presentation/views/favorites_view.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ifood/core/utils/app_colors.dart';
+import 'package:ifood/core/utils/app_text_styles.dart';
 
 class FavoritesView extends StatelessWidget {
   const FavoritesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Favorites View'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
+        title: Text(
+          'Favorites',
+          style: AppStyles.style28w700.copyWith(
+            color: AppColors.blackColor,
+          ),
+        ),
+      ),
+      body: const Column(
+        children: [],
       ),
     );
   }
